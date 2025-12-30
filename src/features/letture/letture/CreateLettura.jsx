192147414e4731da35057f337b57197765864520
Ivano Saracino <saracino.g.ivano@gmail.com>
2025-12-30 13:03:50 +0100

    aggiunto pulsante di aggiunta da detail opera a create opera 2

--- src/features/letture/letture/CreateLettura.jsx
+++ src/features/letture/letture/CreateLettura.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import AsyncSelect from "react-select/async";
 import { useNavigate, useLocation } from "react-router-dom"; 
 import Navbar from "../../../components/Navbar";
