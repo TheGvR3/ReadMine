cf73ea2d4a90c14c70a72ee7c8222c291e42f28b
Ivano Saracino <saracino.g.ivano@gmail.com>
2025-12-30 11:41:24 +0100

    correzione link create lettura

--- src/features/letture/letture/ListLetture.jsx
+++ src/features/letture/letture/ListLetture.jsx
@@ -64,7 +64,7 @@ function ListLetture() {
       <div className="max-w-7xl mx-auto px-4 pt-10">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-3xl font-extrabold text-gray-800">📖 Il Mio Diario</h1>
-          <Link to="/opere" className="bg-indigo-600 text-white px-4 py-2 rounded-lg font-bold">
+          <Link to="/createlettura" className="bg-indigo-600 text-white px-4 py-2 rounded-lg font-bold">
             + Nuova Lettura
           </Link>
         </div>
