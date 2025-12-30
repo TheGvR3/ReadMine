d0765aeb0cd9419fc8b73c9923663b3b776fa04c
Ivano Saracino <saracino.g.ivano@gmail.com>
2025-12-31 00:10:21 +0100

    fix navbar e set user 2

--- src/features/Home.jsx
+++ src/features/Home.jsx
@@ -98,7 +98,7 @@ function Home() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar setError={setError} />
+      <Navbar setUser={setUser} setError={setError} />
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
         {/* Intestazione */}
