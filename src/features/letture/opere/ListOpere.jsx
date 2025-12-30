59263374d62569d78b02b49ae9c7e0d612b951c7
Ivano Saracino <saracino.g.ivano@gmail.com>
2025-12-30 18:23:38 +0100

    aggiornamento endpoint list letture con aggiunta /utente/

--- src/features/letture/opere/ListOpere.jsx
+++ src/features/letture/opere/ListOpere.jsx
@@ -61,7 +61,7 @@ function ListOpere() {
         if (currentUserId) {
           fetchPromises.push(
             secureFetch(
-              `${import.meta.env.VITE_API_BASE_URL}/letture/${currentUserId}`,
+              `${import.meta.env.VITE_API_BASE_URL}/letture/utente/${currentUserId}`,
               {},
               navigate
             )
